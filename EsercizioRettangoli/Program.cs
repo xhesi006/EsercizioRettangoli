37d59cc43d00bc37536eed243c447976674a42e3
nipol <nipol@LAPTOP-5ALMU75U>
2024-03-06 15:19:33 +0100

    Aggiungere i file di progetto.

---
+++ EsercizioRettangoli/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioRettangoli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rettangolo r1 = new Rettangolo(2, 3);
+            Rettangolo r2 = new Rettangolo(3, 5);
+            if (true)
+                Console.WriteLine("rettangoli uguali");
+            else
+                Console.WriteLine("rettangoli diversi");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EsercizioRettangoli/Rettangolo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioRettangoli
+{
+    internal class Rettangolo
+    {
+        private readonly int _base;
+        private readonly int _altezza;
+        public int B { get { return _base; } }
+        public int A { get { return _altezza; } }
+        public Rettangolo(int Base, int Altezza)
+        {
+            _base = Base;
+            _altezza = Altezza;
+        }
+
+        public static bool operator ==(Rettangolo r1, Rettangolo r2)
+        {
+            if (object.ReferenceEquals(r1, null) && object.ReferenceEquals(r2, null))
+                Console.WriteLine("uguali");
+                return true;
+            if (object.ReferenceEquals(r1, null) || object.ReferenceEquals(r2, null))
+                Console.WriteLine("diversi");
+                return false;
+            return (r1.B == r2.B && r1.A == r2.A);
+        }
+        public static bool operator !=(Rettangolo r1, Rettangolo r2)
+        {
+            return !(r1 == r2);
+        }
+
+    }
+}
